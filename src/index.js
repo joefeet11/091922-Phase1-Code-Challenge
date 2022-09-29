47dc4787fc9e93ffd04b50ab176f8882b5c56c24
Kevin Olivier <kevinolivier1111@gmail.com>
2022-09-29 10:37:10 -0600

    submission with deliverables completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 // Your code here
+// initial fetch 
+const fetchResource = url => {
+    return fetch(url)
+    .then(res => res.json())
+}
+//constant variables
+let characterBar = document.getElementById('character-bar')
+let currentVotes = 0
+let voteForm = document.getElementById('votes-form')
+const charVotes = document.getElementById('vote-count')
+//render char name
+function renderCharacterName(characters) {
+    characters.forEach(character => {
+        const charSpan = document.createElement('span')
+        charSpan.textContent = character.name
+        characterBar.appendChild(charSpan)
+        charSpan.addEventListener('click', () => {
+            const charImg = document.getElementById('image')
+            const charName = document.getElementById('name')
+            
+
+            charImg.src = character.image
+            charName.textContent = character.name
+            charVotes.textContent = character.votes
+        }) 
+
+    })
+}
+//event listerners
+voteForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let enterVotes = parseInt(e.target.votes.value)
+    currentVotes += enterVotes
+    charVotes.textContent = currentVotes
+    console.log(e)
+    
+})
+
+//function invocations
+fetchResource('http://localhost:3000/characters')
+.then(characters => {renderCharacterName(characters)})
